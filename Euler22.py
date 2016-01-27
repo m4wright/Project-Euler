af1255a9df3331afed210d2acce3ee0e020901e0
Mathew Wright <m4_wright@hotmail.com>
2016-01-26 19:25:31 -0500

    Create Euler22.py

---
+++ Euler22.py
@@ -0,0 +1,2 @@
+# Program to find the total score of all names in a file (see https://projecteuler.net/problem=22 for file), where
+# the score of a name is the sum of the numeric value for a character multiplied by its position when sorted, where 'a' = 1, 'b' = 2, etc
