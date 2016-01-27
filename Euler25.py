19b7761137d62b3f022634c643b27d5af349f952
Mathew Wright <m4_wright@hotmail.com>
2016-01-26 22:25:02 -0500

    Update Euler25.py

--- Euler25.py
+++ Euler25.py
@@ -1,9 +1,9 @@
 # Program to find the index of the first term in the fibonacci sequence to contain 1000 digits
 
-fibonacci = [1,1]
+fib1, fib2 = 1, 1
 i = 2
-while len(str(fib[-1])) < 1000:
-	fib.append(fib[-1] + fib[-2])
+while len(str(fib2)) < 1000:
+	fib1, fib2 = fib2, fib1 + fib2
 	i += 1
 	
 print(i)
