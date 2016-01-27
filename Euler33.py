58e497eecb6015beefbf727f41b35db574b835b6
Mathew Wright <m4_wright@hotmail.com>
2016-01-27 00:01:14 -0500

    Update Euler33.py

--- Euler33.py
+++ Euler33.py
@@ -1,36 +1,43 @@
 # See https://projecteuler.net/problem=33 for question
 
-from isPrime import isPrime
+import GCD
 import time
 
-def isTrunctPrime(n):
-    number = []
-    for i in str(n):
-        number.append(int(i))
-    test_num = number[:]
-    while len(test_num) > 0:
-        if isPrime(int(''.join(map(str, test_num)))) == False:
-            return False
-        else:
-            test_num.pop()
-    test_num = number[:]
-    test_num.pop(0)
-    while len(test_num) > 0:
-        if not isPrime(int(''.join(map(str, test_num)))):
-            return False
-        else:
-            test_num.pop(0)
+tot_num = 1
+tot_denom = 1
+count = 0
 
-    return True
-            
+start = time.time()
 
-num = 0
-i = 10
-total = 0
-while num < 11:
-    if isTrunctPrime(i):
-        total += i
-        num += 1
-    i += 1
+while count < 4:
+    for num in range(11, 100):
+        for denom in range(num+1, 100):
+            #print(num, denom)
+            denom_list = []
+            num_list = []
+            for i in str(num):
+                num_list.append(int(i))
+            for i in str(denom):
+                denom_list.append(int(i))
+            if str(num)[0] in str(denom) and str(num)[0] != '0':
+                denom_list.remove(num_list[0])
+                num_list.pop(0)
+                if num_list[0] != 0 and denom_list[0] != 0:
+                    if num/denom == num_list[0]/denom_list[0]:
+                        tot_num *= num
+                        tot_denom *= denom
+                        count += 1
+                        print(num, denom)
+            elif str(num)[1] in str(denom) and str(num)[1] != '0':
+                denom_list.remove(num_list[1])
+                num_list.pop(1)
+                if num_list[0] != 0 and denom_list[0] != 0:
+                    if num/denom == num_list[0]/denom_list[0]:
+                        tot_num *= num
+                        tot_denom *= denom
+                        print(num, denom)
+                        count += 1
 
-print(total)
+print(tot_num, tot_denom)
+print(GCD.reduce_fraction(tot_num, tot_denom))
+print(time.time()-start)
